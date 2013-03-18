702ec88498d8dd87412489dd113bfc95c2ccd33d
mimori hiroshi <mimori.hiroshi@butterfly-corp.jp>
2013-03-18 16:21:49 +0900

    handle /_ah/channel/connected and /_ah/channel/disconnected/ please check app.yaml

--- channel/channel.go
+++ channel/channel.go
@@ -11,6 +11,9 @@ import (
 )
 
 func init() {
+
+	http.HandleFunc("/_ah/channel/connected/", connected)
+	http.HandleFunc("/_ah/channel/disconnected/", disconnected)
 	http.HandleFunc("/", main)
 	http.HandleFunc("/receive", receive)
 }
@@ -38,8 +41,52 @@ func main(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// func dataStore() {
+// 	item := &memcache.Item{
+// 		Key:   "lyric",
+// 		Value: []byte("Oh, give me a home"),
+// 	}
+// 	if err := memcache.Add(c, item); err == memcache.ErrNotStored {
+// 		c.Infof("item with key %q already exists", item.Key)
+// 	} else if err != nil {
+// 		c.Errorf("error adding item: %v", err)
+// 	}
+
+// 	// Change the Value of the item
+// 	item.Value = []byte("Where the buffalo roam")
+// 	// Set the item, unconditionally
+// 	if err := memcache.Set(c, item); err != nil {
+// 		c.Errorf("error setting item: %v", err)
+// 	}
+
+// 	// Get the item from the memcache
+// 	if item, err := memcache.Get(c, "lyric"); err == memcache.ErrCacheMiss {
+// 		c.Infof("item not in the cache")
+// 	} else if err != nil {
+// 		c.Errorf("error getting item: %v", err)
+// 	} else {
+// 		c.Infof("the lyric is %q", item.Value)
+// 	}
+// }
+
 func receive(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
 	key := r.FormValue("g")
 	channel.Send(c, key, "go receive!"+time.Now().String())
+}
+
+func connected(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	key := r.FormValue("from")
+
+	c.Infof("connected")
+	c.Infof("%s", key)
+}
+
+func disconnected(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	key := r.FormValue("from")
+
+	c.Infof("disconnected")
+	c.Infof("%s", key)
 }
